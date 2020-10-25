3382f96fa51c3530e5c34a9f863f1be9e4cad1ec
tchartchke <chartai.kl@gmail.com>
2020-10-24 19:39:50 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  attr_accessor :lengths
+
+  def initialize(one, two, three)
+    self.lengths = [one, two, three].sort
+  end
+
+  def kind
+    if lengths[0] + lengths[1] <= lengths[2]
+      begin
+        raise TriangleError
+      end
+    else
+      if lengths[0] == lengths[1] && lengths[1] == lengths[2]
+        return :equilateral
+      elsif lengths[1] == lengths[2]
+        return :isosceles
+      else
+        return :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
